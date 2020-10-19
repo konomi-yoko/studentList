41182c7e518339ddf9bf7e2a1c2285cd89c6fe7d
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-10-19 22:00:57 +0900

    土台

---
+++ app.js
@@ -0,0 +1,12 @@
+let studentList = [
+  {name:'えりか',from:'和歌山県',hobby:'ウクレレ'},
+  {name:'めぐ',from:'長野',hobby:'サーフィン'},
+  {name:'ゆたか',from:'千葉',hobby:'映画'},
+  {name:'あい',from:'沖縄',hobby:'旅行'},
+  {name:'しょう',from:'岩手',hobby:'スノーボード'},
+  {name:'ゆうだい',from:'大阪',hobby:'競艇'},
+  {name:'このみ',from:'千葉',hobby:'ディズニー'},
+  {name:'もえぴ',from:'山形',hobby:'韓ドラ鑑賞'},
+  {name:'ゆうこりん',from:'沖縄',hobby:'写真'},
+  {name:'ルイス',from:'ペルー',hobby:'世間話'}
+];
